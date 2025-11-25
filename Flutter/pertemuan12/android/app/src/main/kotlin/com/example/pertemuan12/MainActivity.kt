b9f474c43bdefa762c59f3e00263f265c886cccd
teukurkpp <raka19495@gmail.com>
2025-11-25 19:54:58 +0700

    upload pertemuan 12

---
+++ Flutter/pertemuan12/android/app/src/main/kotlin/com/example/pertemuan12/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pertemuan12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Flutter/pertemuan12/lib/login_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    String username = _usernameController.text.trim();
+    String password = _passwordController.text;
+
+    if ((username == 'admin' && password == '123') ||
+        (username == 'user' && password == '456')) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const ApiPage()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Username atau password salah')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: _login, child: const Text('Login')),
+          ],
+        ),
+      ),
+    );
+  }
+}
